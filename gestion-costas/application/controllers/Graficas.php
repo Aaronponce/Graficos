e7a3de722e11bff906a54a2cfe740d1f2b7bc018
Aaron Ponce Sandoval <aaron.c.ponce@gmail.com>
2017-01-05 14:44:56 -0300

    Base para la creacion de Graficos

---
+++ gestion-costas/application/controllers/Graficas.php
@@ -0,0 +1,28 @@
+<?php
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Clase controladora de Graficas
+*/
+class Graficas extends CI_Controller{
+	
+	function __construct(){
+		parent::__construct();
+		$this->load->model('Conexion');
+	}
+
+	function index(){
+		$this->load->view('grafico');
+	}
+
+	function ajax(){
+		$query = $this->Conexion->obtenerdatos();
+		if($query != false){
+			foreach ($query->result_array() as $row) {
+				$data[] = $row;
+			}
+		}
+	  	echo json_encode($data);
+	}
+
+}
